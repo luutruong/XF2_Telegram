9441f42ae599d2d2c78e73ec947d685618cf3069
Truong Luu <truonglv@outlook.com>
2022-10-02 20:55:55 +0700

    Remove login notify.

--- _output/extension_hint.php
+++ _output/extension_hint.php
@@ -3,11 +3,6 @@
 // ################## THIS IS A GENERATED FILE ##################
 // DO NOT EDIT DIRECTLY. EDIT THE CLASS EXTENSIONS IN THE CONTROL PANEL.
 
-namespace Truonglv\Telegram\XF\Admin\Controller
-{
-	class XFCP_Login extends \XF\Admin\Controller\Login {}
-}
-
 namespace Truonglv\Telegram\XF\Entity
 {
 	class XFCP_PaymentProviderLog extends \XF\Entity\PaymentProviderLog {}
